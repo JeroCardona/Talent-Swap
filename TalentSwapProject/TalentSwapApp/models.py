604bc82249d06acd6b959abe0a665ac0d487856d
Jk <juanandrs.montoyagaleano@gmail.com>
2024-03-14 21:57:16 -0500

    Comment model + migrate

--- TalentSwapProject/TalentSwapApp/models.py
+++ TalentSwapProject/TalentSwapApp/models.py
@@ -9,3 +9,17 @@ class Vacancy(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+class Comment(models.Model):
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.author)
+    
